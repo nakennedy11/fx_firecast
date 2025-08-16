7d0e26a676d06bb0c433ae59b33cb2c0be2f995c
Noah Kennedy <nak@localhost.localdomain>
2025-08-16 16:04:35 -0400

    some todo comments in dial/remote.ts

--- bridge/src/bridge/components/dial/remote.ts
+++ bridge/src/bridge/components/dial/remote.ts
@@ -9,16 +9,23 @@ import type {
 export default class DialRemote implements Remote {
 
         disconnect(): void {
-            //todo
+            // TODO -- not sure if this actually needs to do anything
         }
 
         sendMediaMessage(message: SenderMediaMessage): void {
             let message1 = message;
-            //todo
+            // TODO
+            // sends a message type to the receiver (DIAL server/first screen device)
+            // currently it's based on the Castv2 API
+            // will need to translate it and abstract it to work with more specific
+            // implementations for different apps (YouTube, Plex, etc[?])
+            // CONTROLS THE MEDIA PLAYBACK/QUEUE/ETC
         }
 
         sendReceiverMessage(message: DistributiveOmit<SenderMessage, "requestId">): void {
             let message1 = message;
-            //todo
+            // TODO
+            // sends a message type like LAUNCH, STOP, GET_STATUS, GET_APP_AVAILABILITY, SET_VOLUME
+            // CONTROLS THE DEVICE ITSELF
         }
 }
